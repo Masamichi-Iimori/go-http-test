7b70a240d5c42aaeea0772eff67b030f12b5b651
飯盛正慶 <iimorimasamichi@masamichi.local>
2021-04-04 18:54:15 +0900

    fix message

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hi, I'm masamichhhi!\n")
+		fmt.Fprint(w, "Hi, I'm masamichhi!\n")
 	})
 	http.ListenAndServe(":80", nil)
 }
