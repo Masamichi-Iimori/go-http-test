3e40d1c7976712da528e13fae4da012ad9590b0b
飯盛正慶 <iimorimasamichi@masamichi.local>
2021-04-04 14:17:08 +0900

    fix: buildspec

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hi, This is New Version!\n")
+		fmt.Fprint(w, "Hi, This is latest Version!\n")
 	})
 	http.ListenAndServe(":80", nil)
 }
