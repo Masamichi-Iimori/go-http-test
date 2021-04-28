c14f6f8bc678d3c83de639011470619d0d8cc705
飯盛正慶 <masamichhhi@gmail.com>
2021-04-28 12:18:49 +0900

    fix migration

--- src/db/migrations/1_user.up.sql
+++ src/db/migrations/1_user.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE `users` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `age` int NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `deleted_at` datetime,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `deletedAt` datetime,
   PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8mb4;
